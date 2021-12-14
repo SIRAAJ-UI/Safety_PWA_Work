9b8f8bbb755ecce0fad81014a6b6a1e84f001303
Sirajudeen <ksiraj2002@gmail.com>
2021-12-15 00:46:56 +0530

    latest update.

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -2,18 +2,22 @@
 import { AccountService } from './_services';
 import { User } from './_models';
 import { ConnectionService } from 'ng-connection-service';
+import IdleTimer from "./IdleTimer";
 
 
 @Component({ selector: 'app', templateUrl: 'app.component.html' })
 export class AppComponent implements OnInit {
     user: User;
-    public isOnline: boolean =  window.navigator.onLine;
+    public isOnline: boolean = window.navigator.onLine;
     hasNetworkConnection: boolean;
     hasInternetAccess: boolean;
+    private timer: IdleTimer;
 
     constructor(private accountService: AccountService, private connectionService: ConnectionService) {
         this.accountService.user.subscribe(x => this.user = x);
+
     }
+
 
     ngOnInit() {
         this.connectionService.monitor().subscribe((currentState: any) => {
@@ -28,11 +32,18 @@ export class AppComponent implements OnInit {
             }
         });
         this.accountService.SetIsOnline(this.isOnline);
-        console.log(this.isOnline);
+        this.timer = new IdleTimer({
+            timeout: 60*30, //expired after 10 secs
+            onTimeout: () => {
+                this.accountService.logout();
+            }
+        });
     }
 
-     
-      
+
+    ngOnDestroy() {
+        this.timer.clear();
+    }
     logout() {
         this.accountService.logout();
     }
--- frontend/src/app/navbar/navbar.component.ts
+++ frontend/src/app/navbar/navbar.component.ts
@@ -4,19 +4,20 @@ import { AccountService } from '@app/_services';
 import { ConnectionService } from 'ng-connection-service';
 
 @Component({
-  selector: 'app-navbar',
-  templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.scss']
+    selector: 'app-navbar',
+    templateUrl: './navbar.component.html',
+    styleUrls: ['./navbar.component.scss']
 })
 export class NavbarComponent implements OnInit {
 
-  user: any;
-  AssignedReportUrl: string = "";
-  ReportedReportUrl: string = "";
+    user: any;
+    AssignedReportUrl: string = "";
+    ReportedReportUrl: string = "";
 
     public isOnline: boolean = window.navigator.onLine;
     hasNetworkConnection: boolean;
     hasInternetAccess: boolean;
+    public showmenu: boolean = false;
     constructor(private accountService: AccountService, private connectionService: ConnectionService) {
         this.accountService.user.subscribe(x => {
             this.user = x;
@@ -44,4 +45,7 @@ export class NavbarComponent implements OnInit {
         this.accountService.logout();
     }
 
+    ShowHideMenu() {
+        this.showmenu = !this.showmenu;
+    }
 }
